77e38b42d2055b59e8174d7a16cba114c53e3234
MertTatlii <merttatli1605@gmail.com>
2024-07-17 15:01:49 +0300

    First one solved

---
+++ 1thQ.py
@@ -0,0 +1,17 @@
+def divisible(x):
+    if x%3==0 or x%5==0:
+        return True
+    else:
+        return False
+
+numberlist= [i for i in range(2,1000)]
+print(numberlist)
+summary=0
+for i in numberlist:
+    if divisible(i):
+        summary +=i
+print(summary)
+
+
+
+
