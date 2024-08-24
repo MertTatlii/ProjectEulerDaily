84ba49b3f2b4950397c9275405a074735ff85361
Mert Tatli <merttatli1605@gmail.com>
2024-08-24 14:00:24 +0300

    16th question

---
+++ 16thQ.py
@@ -0,0 +1,11 @@
+num = 2**1000
+numnum= num
+sumnumnum = 0
+while numnum>0:
+    basamak = numnum % 10
+    sumnumnum = sumnumnum + basamak
+    numnum = numnum // 10
+
+
+print(num)
+print(sumnumnum)
